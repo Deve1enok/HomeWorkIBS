4eae6b0e986a1d2d17c3ed626369b8d45eea6fa3
Dinar <dinar-97@mail.ru>
2024-04-07 13:20:04 +0300

    added new data

--- src/test/java/org/ibs/fazlyakhmetov/data/DataFakeAssertResults.java
+++ src/test/java/org/ibs/fazlyakhmetov/data/DataFakeAssertResults.java
@@ -1,14 +1,15 @@
 package org.ibs.fazlyakhmetov.data;
 
 /**
- * Тестовые данные для проверки результата
+ * Тестовые данные для проверки результата в таблице
  *
  * @author Fazlyakhmetov_Dinar
  */
 public class DataFakeAssertResults {
-    public final String dataForCheckVeg = "5 Картофель Овощ false";
-    public final String dataForCheckExoticVeg = "5 Кивано Овощ true";
-    public final String dataForCheckExoticFruit = "5 Манго Фрукт true";
-    public final String dataForCheckFruit = "5 Груша Фрукт false";
+    public final String notExoticVegResult = "5 Картофель Овощ false";
+    public final String exoticVegResult = "5 Кивано Овощ true";
+    public final String exoticFruitResult = "5 Манго Фрукт true";
+    public final String notExoticFruitResult = "5 Груша Фрукт false";
+    public final String defaultFruitNum4 = "4 Яблоко Фрукт false";
 
 }
