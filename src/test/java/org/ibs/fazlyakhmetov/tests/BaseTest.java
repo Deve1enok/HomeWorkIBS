8310eac9a0721da2c84b94d748e501d1675e9908
Dinar <dinar-97@mail.ru>
2024-04-11 12:02:52 +0300

    fix timeout driver

--- src/test/java/org/ibs/fazlyakhmetov/tests/BaseTest.java
+++ src/test/java/org/ibs/fazlyakhmetov/tests/BaseTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class BaseTest {
     public static WebDriver driver;
@@ -18,8 +18,8 @@ public class BaseTest {
         System.setProperty("webdriver.chromedriver.driver", configOwner.chromeDriver());
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.get(configOwner.baseUrl());
 
     }
